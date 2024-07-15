cc4649700547a5e259f51e60a39ad8e04d0d4905
Ibrahim.Osama <iosama.amin@gmail.com>
2024-07-15 19:13:10 +0300

    edited main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,5 @@
 terraform {
     required_version = ">= 0.12"
-    backend "s3" {
-        bucket = "myapp-bucket"
-        key = "myapp/state.tfstate"
-        region = "eu-west-3"
-    }
 }
 
 provider "aws" {
