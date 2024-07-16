cae73c6202a44fd7cf40e76691746efea173e689
Ibrahim.Osama <iosama.amin@gmail.com>
2024-07-16 10:23:34 +0300

    another way to install docker compose

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -4,5 +4,5 @@ sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 
 # install docker-compose 
-sudo yum install docker-compose-plugin -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
