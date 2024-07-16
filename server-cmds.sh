5077912dbea4bc99e599923a9f1657b8a19ab906
Ibrahim.Osama <iosama.amin@gmail.com>
2024-07-16 14:58:12 +0300

    trying to fix this shit script

--- server-cmds.sh
+++ server-cmds.sh
@@ -2,11 +2,11 @@
 
 echo "Setting Environmental variables"
 
-DOCKER_USER="$1"
-DOCKER_PWD="$2"
+#DOCKER_USER="$1"
+#DOCKER_PWD="$2"
 
 echo "logging into docker"
-echo "$DOCKER_PWD" | sudo docker login -u "$DOCKER_USER" --password-stdin
+echo "$2" | sudo docker login -u "$1" --password-stdin
 
 echo "Starting docker compose now"
 sudo docker-compose -f docker-compose.yaml --env-file docker-compose.env up --detach
