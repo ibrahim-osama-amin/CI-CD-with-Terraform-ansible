675f4bffef7d8fc630909c4af0145274f8a06fd5
Ibrahim.Osama <iosama.amin@gmail.com>
2024-07-16 10:57:31 +0300

    editing the bash script again

--- server-cmds.sh
+++ server-cmds.sh
@@ -4,10 +4,10 @@ echo "Setting Environmental variables"
 
 export IMAGE=$1
 export DOCKER_USER=$2
-export DOCKER_PWD='$3'
+export DOCKER_PWD="$3"
 
 echo "logging into docker"
-echo $DOCKER_PWD | sudo docker login -u $DOCKER_USER --password-stdin
+echo "$DOCKER_PWD" | sudo docker login -u "$DOCKER_USER" --password-stdin
 
 echo "Starting docker compose now"
 sudo docker-compose -f docker-compose.yaml up --detach
