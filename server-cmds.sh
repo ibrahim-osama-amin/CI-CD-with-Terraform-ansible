5985b12a9c0227f5465d363752e2f01e3093b07b
Ibrahim.Osama <iosama.amin@gmail.com>
2024-07-16 14:50:29 +0300

    testing again

--- server-cmds.sh
+++ server-cmds.sh
@@ -2,8 +2,8 @@
 
 echo "Setting Environmental variables"
 
-export DOCKER_USER="$1"
-export DOCKER_PWD="$2"
+DOCKER_USER="$1"
+DOCKER_PWD="$2"
 
 echo "logging into docker"
 echo "$DOCKER_PWD" | sudo docker login -u "$DOCKER_USER" --password-stdin
